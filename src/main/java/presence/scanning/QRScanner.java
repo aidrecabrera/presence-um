758893115cda2c2b0fdaaa5096576189d444a579
Svene <cabreraaidre@gmail.com>
2023-03-01 13:59:07 +0800

    ZXING + WEBCAM (WORKING)

---
+++ src/main/java/presence/scanning/QRScanner.java
@@ -0,0 +1,118 @@
+package presence.scanning;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamResolution;
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class QRScanner extends JFrame implements Runnable {
+
+    private static final long serialVersionUID = 1L;
+
+    private final MultiFormatReader reader = new MultiFormatReader();
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+    private final JPanel panel = new JPanel();
+    private final JLabel label = new JLabel();
+
+    private final Webcam webcam = Webcam.getDefault();
+    private final Dimension size = WebcamResolution.VGA.getSize();
+
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public QRScanner() {
+        super("QR Scanner");
+
+        this.panel.setLayout(new BorderLayout());
+        this.panel.add(this.label, BorderLayout.CENTER);
+
+        this.setContentPane(this.panel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+        this.webcam.setViewSize(size);
+        this.webcam.open();
+
+        this.reader.setHints(null);
+        this.executor.scheduleAtFixedRate(this, 0, 33, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void run() {
+        BufferedImage image = this.webcam.getImage();
+
+        if (image == null) {
+            return;
+        }
+
+        // Convert the image to grayscale.
+        Mat mat = new Mat();
+        Imgproc.cvtColor(MatUtil.bufferedImageToMat(image), mat, Imgproc.COLOR_BGR2GRAY);
+
+        // Apply Gaussian blur to the image to reduce noise.
+        Imgproc.GaussianBlur(mat, mat, new org.opencv.core.Size(3, 3), 0);
+
+        try {
+            BufferedImage bufferedImage = MatUtil.matToBufferedImage(mat);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(new BufferedImageLuminanceSource(bufferedImage)));
+            Result result = this.reader.decode(bitmap);
+            if (result != null) {
+                System.out.println(result.getText());
+                this.label.setText(result.getText());
+            }
+        } catch (Exception e) {
+            // No QR code found in the image.
+        }
+
+        // Convert the grayscale image back to a BufferedImage.
+        BufferedImage outputImage = MatUtil.matToBufferedImage(mat);
+        this.label.setIcon(new ImageIcon(outputImage));
+    }
+
+
+    public class MatUtil {
+
+        public static Mat bufferedImageToMat(BufferedImage bi) {
+            Mat mat = new Mat(bi.getHeight(), bi.getWidth(), CvType.CV_8UC3);
+            byte[] data = ((DataBufferByte) bi.getRaster().getDataBuffer()).getData();
+            mat.put(0, 0, data);
+            return mat;
+        }
+
+        public static BufferedImage matToBufferedImage(Mat mat) {
+            int type = BufferedImage.TYPE_BYTE_GRAY;
+            if (mat.channels() > 1) {
+                type = BufferedImage.TYPE_3BYTE_BGR;
+            }
+            BufferedImage bi = new BufferedImage(mat.width(), mat.height(), type);
+            byte[] data = ((DataBufferByte) bi.getRaster().getDataBuffer()).getData();
+            mat.get(0, 0, data);
+            return bi;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new QRScanner());
+    }
+
+}
