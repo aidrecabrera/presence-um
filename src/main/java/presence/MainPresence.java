1c9337eb45c1222e17f4ad2db0af463ae0c4f0cf
Svene <cabreraaidre@gmail.com>
2023-03-02 20:04:50 +0800

    Dashboard Initialization

--- src/main/java/presence/MainPresence.java
+++ src/main/java/presence/MainPresence.java
@@ -3,7 +3,6 @@ package presence;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +10,10 @@ import java.io.IOException;
 public class MainPresence extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Application.setUserAgentStylesheet(Application.STYLESHEET_MODENA);
         FXMLLoader fxmlLoader = new FXMLLoader(MainPresence.class.getResource("MainPresenceUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.setResizable(false);
         stage.setTitle("UM Presence");
         stage.setScene(scene);
         stage.show();
